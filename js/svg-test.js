fb83c736d883a5305936e048bc0693f975c72c6c
r5w <robert.wiltshire@gmail.com>
2014-06-02 20:38:39 +0100

    added js working file

---
+++ js/svg-test.js
@@ -0,0 +1,4 @@
+// First lets create our drawing surface out of existing SVG element
+// If you want to create new surface just provide dimensions
+// like s = Snap(800, 600);
+var s = Snap("#svg");
