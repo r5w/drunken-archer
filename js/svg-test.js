4c83f5e425ba53213aadc3e41ba20a901c5abbb0
r5w <robert.wiltshire@gmail.com>
2014-06-02 21:15:05 +0100

    draggable items test

--- js/svg-test.js
+++ js/svg-test.js
@@ -1,4 +1,21 @@
 // First lets create our drawing surface out of existing SVG element
 // If you want to create new surface just provide dimensions
 // like s = Snap(800, 600);
-var s = Snap("#svg");
+function init(){
+	var s = Snap("#svg");
+	var bigCircle = s.circle(150,150,100);
+	bigCircle.attr({
+		fill: 'red',
+		stroke: 'white',
+		strokeWidth: 4
+	})
+	bigCircle.drag();
+	var mediumCircle = s.circle(400,150,75);
+	mediumCircle.attr({
+		fill: 'yellow',
+		stroke: 'brown',
+		strokeWidth: 4
+	})
+	mediumCircle.drag();
+}
+window.onload = init;
