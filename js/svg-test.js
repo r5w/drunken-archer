47f450a9973a8128f5cd0f7b99b36a975aa65d21
r5w <robert.wiltshire@gmail.com>
2014-06-03 22:37:44 +0100

    clickable demo

--- js/svg-test.js
+++ js/svg-test.js
@@ -2,20 +2,81 @@
 // If you want to create new surface just provide dimensions
 // like s = Snap(800, 600);
 function init(){
-	var s = Snap("#svg");
-	var bigCircle = s.circle(150,150,100);
+	var s = Snap.select("#svg");
+	var printmessage = function(string){
+		console.log(string);
+	};
+	var bg = s.rect(0,0,400,400);
+	bg.attr({fill:'#ddd'});
+	/*
+	bigCircle.drag();
+	var mediumCircle = s.circle(100,150,75);
+	mediumCircle.attr({
+		fill: 'yellow',
+		stroke: 'brown',
+		strokeWidth: 4
+	});
+	var message = "HELLO WORLD!"
+	var t1 = s.text(50,50,"CHEESE!!!");
+	t1.attr({text: message});
+	t1.attr({textpath: "M10,10L100,100"});
+	var g = s.group(mediumCircle,bigCircle);
+	Snap.animate(0,200, function(val){
+		bigCircle.attr({radius:val});
+	}, 10000);
+	//mediumCircle.drag();
+*/
+	var colours = [ "#DFFFBE",
+					"#118EE8",
+					"#FF1FAD",
+					"#E8D5A4",
+					"#12FF52",
+					"#ff5000",
+					"#000000"
+	]
+
+	var numberOfSides 	= colours.length,
+		size			= 30,
+		Xcentre			= (s.getBBox().width)/2,
+		Ycentre			= (s.getBBox().height)/2
+
+
+console.log(Xcentre);
+	var bigCircle = s.circle(Xcentre,Ycentre,(size*2));
 	bigCircle.attr({
 		fill: 'red',
 		stroke: 'white',
 		strokeWidth: 4
 	})
-	bigCircle.drag();
-	var mediumCircle = s.circle(400,150,75);
-	mediumCircle.attr({
-		fill: 'yellow',
-		stroke: 'brown',
-		strokeWidth: 4
-	})
-	mediumCircle.drag();
+	bigCircle.click(function(){
+		printmessage(this.node.r.baseVal.value);
+	});
+		var dotgroup = [];
+		var papergroup = s.g();
+
+	for (var i=0;i<=numberOfSides-1;i+=1){
+		dotgroup.push(i);
+		newpointX = Xcentre + size*3 * Math.cos(i * 2 * Math.PI / numberOfSides)
+		newpointY = Ycentre + size*3 * Math.sin(i * 2 * Math.PI / numberOfSides)
+		dotgroup[i] = s.circle(newpointX,newpointY,size);
+		dotgroup[i].id = i;
+		dotgroup[i].attr({ fill:colours[i]});
+		dotgroup[i].click(function(elem){
+			//var this = dotgroup[i];
+			//console.log(elem);
+			var thisx = elem.target.cx.baseVal.value;
+			var thisy = elem.target.cy.baseVal.value;
+			var thisid = this.id;
+			printmessage("x: " + thisx + " y: " + thisy + " ID: " + thisid);
+		})
+		papergroup.add(dotgroup[i]);
+		//console.log(dotgroup[i]);
+	}
+	papergroup.animate({transform:'r360,Xcentre,Ycentre'},2000,mina.easeinout);
+	papergroup.drag();
+
+
+
+
 }
 window.onload = init;
