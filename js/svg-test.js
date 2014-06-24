fb29f2b26c825b92164817c973a9cf4b920b038c
Rob Wiltshire <robert.wiltshire@gmail.com>
2014-06-24 23:09:34 +0100

    click events updated

--- js/svg-test.js
+++ js/svg-test.js
@@ -12,6 +12,8 @@ function init(w, h) {
 		howWide = w,
 		howHigh = h,
 		bg = s.rect(0, 0, howWide, howHigh),
+		cursor,
+		cursorg,
 		i,
 /*		colours = [
 			"#111111", //blue
@@ -41,6 +43,7 @@ function init(w, h) {
 		dotgroup,
 		newpointX,
 		newpointY,
+		newPointArr,
 		papergroup;
 	//bg = s.rect(0, 0, 400, 400);
 	bg.attr({fill: '#fff'});
@@ -64,40 +67,92 @@ function init(w, h) {
 */
 	console.log(Xcentre);
 	bigCircle = s.circle(Xcentre, Ycentre, (size * 4.5));
+	cursor = s.circle(Xcentre + size * 3, Ycentre, (size / 2));
 	bigCircle.attr({
 		fill: '#fff',
 		stroke: '#eee',
 		strokeWidth: 2
 	});
+	
+	cursor.attr({
+		fill: '#0f0',
+		stroke: '#333',
+		strokeWidth: 20
+	});	
+	//cursor.attr({cx:180,cy:180});
 	bigCircle.click(function () {
 		printmessage(this.node.r.baseVal.value);
 	});
 
+
+
+    cursorg = s.g( bigCircle, cursor);	
 	dotgroup = [];
+	newPointArr = [];
 	papergroup = s.g();
 
 	function float2int (value) {
 		return value | 0;
 	}
+	function setCursorPoint(x,y) {
+		//currentx = cursorg.cx;
+		//currenty = cursorg.cy;
+		//cursor.attr({cx:x,cy:y});
+		//cursorg.attr({cx:x,cy:y});
+		cursorg.animate({transform: 'r360, x, y'}, 500, mina.easeinout);
+	}
 	function dotclickhandle(e, thing) {
 		return function (thing) {
 			//var data = e.data;
 			//alert(e.data);
+
 			console.log(thing.target.cx.baseVal.value);
-			//console.log(e.target);
+			console.log(e + "x:" + dotgroup[e].thisx + "y:" + dotgroup[e].thisy);
+			//console.log("cursor"+c);
+			setCursorPoint(dotgroup[e].thisx,dotgroup[e].thisy);
 			//	var thisx = elem.target.cx.baseVal.value;
 			//	var thisy = elem.target.cy.baseVal.value;
 			//	var thisid = this.id;
 			//	printmessage("x: " + thisx + " y: " + thisy + " ID: " + thisid);
 		};
 	}
+	function distanceAround(numberOfSidesval,Xcentreval,Ycentreval,diameter,degree) {
+
+		var result,magicvalue;
+		result = [];
+		magicvalue = 3;
+		//x = Xcentreval  ;
+		//console.log("x:"+x);
+		//x = Xcentreval + (diameter/2) ;
+		//x = Xcentreval + diameter * magicvalue;
+		//y = Ycentreval ;
+		//console.log("y:"+y);
+		//y = Ycentreval + (diameter/2) ;
+		//y = Ycentreval + diameter * magicvalue;
+
+		//result.x =  Math.cos(degree * 2 * Math.PI / numberOfSidesval) * Xcentreval * 3 ;
+		//result.y =  Math.sin(degree * 2 * Math.PI / numberOfSidesval) * Ycentreval * 3 ;
+		result.x = Xcentreval + diameter * magicvalue * Math.cos(degree * 2 * Math.PI / numberOfSidesval);
+		result.y = Ycentreval + diameter * magicvalue * Math.sin(degree * 2 * Math.PI / numberOfSidesval);		
+		console.log(result);
+		return result;
+	}
 	for (i = 0; i <= numberOfSides - 1; i += 1) {
 		dotgroup.push(i);
-		newpointX = Xcentre + size * 3 * Math.cos(i * 2 * Math.PI / numberOfSides);
-		newpointY = Ycentre + size * 3 * Math.sin(i * 2 * Math.PI / numberOfSides);
+		//newpointX = Xcentre + size * 3 * Math.cos(i * 2 * Math.PI / numberOfSides);
+		//newpointY = Ycentre + size * 3 * Math.sin(i * 2 * Math.PI / numberOfSides);
+		//console.log(Xcentre);
+		//console.log(Ycentre);
+
+		newPointArr = distanceAround(numberOfSides,Xcentre,Ycentre,size,i);
+		newpointX = newPointArr.x;
+		//console.log(newPointArr);
+		newpointY = newPointArr.y;
 		dotgroup[i] = s.circle(newpointX, newpointY, size);
 		dotgroup[i].id = i;
 		dotgroup[i].attr({ fill: colours[i]});
+		dotgroup[i].thisx =newpointX
+		dotgroup[i].thisy =newpointY
 		/*
 		dotgroup[i].click(function(elem){
 			//var this = dotgroup[i];
@@ -108,7 +163,7 @@ function init(w, h) {
 			//printmessage("x: " + thisx + " y: " + thisy + " ID: " + thisid);
 		})
 */
-		dotgroup[i].click(dotclickhandle(dotgroup[i].id));
+		dotgroup[i].click(dotclickhandle(dotgroup[i].id,cursor));
 		papergroup.add(dotgroup[i]);
 		//console.log(dotgroup[i]);
 	}
